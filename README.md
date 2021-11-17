7d77d61847e861403e1d85db9388459831ea9e3c
Epi Vou <pimenas@auvious.com>
2021-11-17 12:06:42 +0200

    add README and update deprecated code

---
+++ README.md
@@ -0,0 +1,53 @@
+# Auvious Android SDK Example
+This repo contains example of how to use AuviousSDK.
+
+To use AuviousSDK in your project follow these steps:
+- Add `https://nexus.auvious.com/repository/maven-releases` nexus repository. If you are using gradle
+  then your build.gradle repositories section should look something like this:
+  ```groovy
+      repositories {
+        mavenCentral()
+        // ... other repositories
+        // Auvious SDK Repo
+        maven { url "https://nexus.auvious.com/repository/maven-releases" }
+    }
+  ```
+- Include `com.auvious.android:sdk:1.0.7` dependency. Again if you are using Gradle, then your
+  build.gradle dependencies section would look like this:
+  ```groovy
+  dependencies {
+    //... other dependencies
+  
+    // Auvious SDK
+    implementation 'com.auvious.android:sdk:1.0.7'
+  }
+  ```
+
+- Launch `AuviousSimpleConferenceActivity` to join a call with ticket like this:
+  ```kotlin
+  val callOptions = AuviousSimpleConferenceOptions(
+      "customer",
+      "https://auvious.video",
+      "wss://auvious.video/ws",
+      mapOf(
+          "ticket" to edit_ticket.text.toString(),
+          "grant_type" to "password"
+      )
+  )
+  
+  val startForResult =
+      registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+      { result: ActivityResult ->
+          if (result.resultCode != Activity.RESULT_OK) {
+              //  you will get result here in result.data
+              result.data?.getParcelableExtra<AuviousSdkSimpleConferenceError>(
+                  AuviousSimpleConferenceActivity.getResultIntentName()
+              )?.let {
+                  Toast.makeText(this, "Error code is ${it.errorCode}", Toast.LENGTH_LONG)
+                      .show()
+              }
+          }
+          AuviousConferenceSDK.instance.onDestroy()
+      }
+  startForResult.launch(AuviousSimpleConferenceActivity.getIntent(this, callOptions))
+  ```
--- app/src/main/java/com/auvious/auviousproject/MainActivity.kt
+++ app/src/main/java/com/auvious/auviousproject/MainActivity.kt
@@ -2,28 +2,26 @@ package com.auvious.auviousproject
 
 import android.Manifest
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import com.auvious.auvioussdk.core.AuviousConferenceSDK
-import com.auvious.auvioussdk.ui.simpleconference.AuviousSimpleConferenceOptions
-import com.auvious.auvioussdk.ui.simpleconference.AuviousSimpleConferenceActivity
 import com.auvious.auvioussdk.ui.simpleconference.AuviousSdkSimpleConferenceError
+import com.auvious.auvioussdk.ui.simpleconference.AuviousSimpleConferenceActivity
+import com.auvious.auvioussdk.ui.simpleconference.AuviousSimpleConferenceOptions
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
-
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 /**
  * Created by BKarampinis on 13-Jul-20
  */
-class MainActivity: AppCompatActivity() {
-
-    private val simpleConferenceActivityRequestCode = 999
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,64 +34,75 @@ class MainActivity: AppCompatActivity() {
         )
     }
 
-    private fun setupViews(){
+    private fun setupViews() {
 
         /**
          * AuviousSimpleConferenceActivity example
          * */
         button_conference.setOnClickListener {
-            if(edit_ticket.text.isNullOrEmpty()){
-                Toast.makeText(this, "Please supply a valid ticket id to continue", Toast.LENGTH_SHORT).show()
-            }else if(!permissionConfirmed()){
-                Toast.makeText(this, "Please give needed permissions to application first", Toast.LENGTH_SHORT).show()
-            }else{
+            if (edit_ticket.text.isNullOrEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Please supply a valid ticket id to continue",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (!permissionConfirmed()) {
+                Toast.makeText(
+                    this,
+                    "Please give needed permissions to application first",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
                 startSimpleConferenceActivity()
             }
         }
 
     }
 
-    private fun startSimpleConferenceActivity(){
-        val clientId = "customer"
-        val baseEndpoint = "https://auvious.video"
-        val mqttEndpoint = "wss://auvious.video/ws"
-        val params = mapOf(
-            "ticket" to edit_ticket.text.toString(),
-            "grant_type" to "password"
-        )
-
+    private fun startSimpleConferenceActivity() {
         val callOptions = AuviousSimpleConferenceOptions(
-            clientId,
-            baseEndpoint,
-            mqttEndpoint,
-            params
+            "customer",
+            "https://auvious.video",
+            "wss://auvious.video/ws",
+            mapOf(
+                "ticket" to edit_ticket.text.toString(),
+                "grant_type" to "password"
+            )
         )
 
-        startActivityForResult(
-            AuviousSimpleConferenceActivity.getIntent(this, callOptions),
-            simpleConferenceActivityRequestCode
-        )
-
-    }
-
-    private fun permissionConfirmed(): Boolean{
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PERMISSION_GRANTED
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == simpleConferenceActivityRequestCode){
-            if(resultCode != Activity.RESULT_OK){
-                data?.getParcelableExtra<AuviousSdkSimpleConferenceError>(AuviousSimpleConferenceActivity.getResultIntentName())?.let{
-                    Toast.makeText(this, "Error code is ${it.errorCode}", Toast.LENGTH_LONG).show()
+        val startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result: ActivityResult ->
+                if (result.resultCode != Activity.RESULT_OK) {
+                    //  you will get result here in result.data
+                    result.data?.getParcelableExtra<AuviousSdkSimpleConferenceError>(
+                        AuviousSimpleConferenceActivity.getResultIntentName()
+                    )?.let {
+                        Toast.makeText(this, "Error code is ${it.errorCode}", Toast.LENGTH_LONG)
+                            .show()
+                    }
                 }
+                AuviousConferenceSDK.instance.onDestroy()
             }
-            AuviousConferenceSDK.instance.onDestroy()
-        }else{
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+        startForResult.launch(AuviousSimpleConferenceActivity.getIntent(this, callOptions))
     }
 
+    private fun permissionConfirmed(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECORD_AUDIO
+        ) == PERMISSION_GRANTED
+    }
 }
